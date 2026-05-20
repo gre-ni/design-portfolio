e4bb4faf4c57fb93d3f3d8ed399cb07e21d046df
NikolG <greplova.nikol@gmail.com>
2026-05-20 16:59:20 +0200

    init server setup

---
+++ server/app.py
@@ -0,0 +1,4 @@
+from flask import Flask, jsonify
+import sqlite3
+
+app = Flask(__name__)
---
+++ server/app/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS projects (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    slug TEXT UNIQUE NOT NULL,
+    year INTEGER,
+    description TEXT,
+    cover_image_url TEXT,
+    featured BOOLEAN,
+    story TEXT
+);
+
+CREATE TABLE IF NOT EXISTS tags (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE NOT NULL,
+    type TEXT CHECK(type IN ('category', 'tool', 'industry'))
+);
+
+CREATE TABLE IF NOT EXISTS project_tags (
+    project_id INTEGER REFERENCES projects(id),
+    tag_id INTEGER REFERENCES tags(id),
+    PRIMARY KEY (project_id, tag_id)
+);
