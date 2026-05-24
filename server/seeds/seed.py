68ee523a422fefea32c3f2d0158b1b8bbdd23b56
NikolG <greplova.nikol@gmail.com>
2026-05-24 14:33:26 +0200

    seed for tags and relationships

--- server/seeds/seed.py
+++ server/seeds/seed.py
@@ -8,6 +8,20 @@ DB_PATH = Path(__file__).parent.parent / "instance" / "portfolio.db"
 con = sqlite3.connect(DB_PATH)
 con.row_factory = sqlite3.Row
 
+# Tags table
+with open("tags.csv") as f:
+    reader = csv.DictReader(f)
+    db = con.cursor()
+    for row in reader:
+        db.execute("""
+        INSERT INTO tags (name, type) VALUES (?,?)
+        ON CONFLICT(name) DO UPDATE SET
+            name = excluded.name,
+            type = excluded.type
+        """, (row["name"], row["type"]))
+
+
+# Projects table
 query = """INSERT INTO projects (title, slug, year, description, cover_image_url, featured, visible) 
     VALUES (?, ?, ?, ?, ?, ?, ?)
     ON CONFLICT(slug) DO UPDATE SET
@@ -23,10 +37,12 @@ query = """INSERT INTO projects (title, slug, year, description, cover_image_url
 
 with open("projects.csv") as f:
     reader = csv.DictReader(f)
+    db = con.cursor()
     for row in reader:
-        db = con.cursor()
         db.execute(query, (row["title"], row["slug"], row["year"], row["description"], row["cover_image_url"], row["featured"], row["visible"]))
 
+
+# Stories table
 for filename in os.listdir("./projects"):
     if filename.endswith(".md"):
         slug_value = filename[:-3]
@@ -40,6 +56,21 @@ for filename in os.listdir("./projects"):
                 VALUES ((SELECT id FROM projects WHERE slug = ?), ?)
                 ON CONFLICT(project_id) DO UPDATE SET story = excluded.story  
                 """, (slug_value, content))
+
+
+# Project tags relationship table
+with open("projects-tags.csv") as f:
+    reader = csv.DictReader(f)
+    db = con.cursor()
+    for row in reader:
+        db.execute("""
+        INSERT OR IGNORE INTO project_tags (project_id, tag_id)
+        VALUES (
+            (SELECT id FROM projects WHERE slug = ?),
+            (SELECT id FROM tags WHERE name = ?)
+        )
+        """, (row["project"], row["tag"]))
+
 
 con.commit()
 con.close()
