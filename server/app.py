e0c59197363c6b5333049f7f10342bdd8cd0697f
NikolG <greplova.nikol@gmail.com>
2026-05-24 14:06:00 +0200

    general and project story endpoint

--- server/app.py
+++ server/app.py
@@ -1,4 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import sqlite3
+from pathlib import Path
 
 app = Flask(__name__)
+
+DB_PATH = Path(__file__).parent / "instance" / "portfolio.db"
+
+@app.route("/projects", methods=["GET"])
+def projects_all():
+    with sqlite3.connect(DB_PATH) as con: 
+        con.row_factory = sqlite3.Row
+        db = con.cursor()
+        results = db.execute("SELECT * FROM projects").fetchall() # returns list of sqlite row objects
+        return jsonify([dict(row) for row in results]), 200
+        
+
+@app.route("/stories", methods=["GET"])
+def project_story():
+    id = request.args["id"]
+    with sqlite3.connect(DB_PATH) as con: 
+        con.row_factory = sqlite3.Row
+        db = con.cursor()
+        results = db.execute("SELECT * FROM project_stories WHERE project_id = ?", (id,)).fetchall()
+        print(results)
+        return jsonify([dict(row) for row in results]), 200
+    
