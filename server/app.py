57dd73e253de25d1ce986177fef78be18e5de025
NikolG <greplova.nikol@gmail.com>
2026-05-25 12:24:28 +0200

    CORS fix

--- server/app.py
+++ server/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import sqlite3
 from pathlib import Path
 
 app = Flask(__name__)
+CORS(app)
 
 DB_PATH = Path(__file__).parent / "instance" / "portfolio.db"
 
